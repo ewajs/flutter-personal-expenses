946bc1a9b7b02479fa624233c9aab12dfb7d1cb7
ewajs <ezequiel.wajs@gmail.com>
2020-12-12 16:49:01 -0300

    Added theming

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        accentColor: Colors.green,
+      ),
       home: MyHomePage(),
     );
   }
@@ -75,7 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
@@ -90,7 +94,7 @@ class _MyHomePageState extends State<MyHomePage> {
             Card(
               child: Container(
                 width: double.infinity,
-                color: Colors.blue,
+                color: Theme.of(context).primaryColor,
                 child: Text('CARD!'),
               ),
             ),
