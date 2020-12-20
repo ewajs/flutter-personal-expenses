6403b6e5ec834e572f4df0652ae781c338ccd662
ewajs <ezequiel.wajs@gmail.com>
2020-12-20 17:13:58 -0300

    Extracted out Choose Date button to create an adaptive button widget

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,7 +1,8 @@
-import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+
+import './adaptive_flat_button.dart';
 
 class NewTransaction extends StatefulWidget {
   final newTransaction;
@@ -81,20 +82,7 @@ class _NewTransactionState extends State<NewTransaction> {
                             ? 'No Date Chosen'
                             : 'Date: ' +
                                 DateFormat.yMd().format(_selectedDate))),
-                    Platform.isIOS
-                        ? CupertinoButton(
-                            child: Text(
-                              'Choose Date',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            onPressed: _presentDatePicker,
-                          )
-                        : FlatButton(
-                            onPressed: _presentDatePicker,
-                            child: Text('Choose Date',
-                                style: TextStyle(fontWeight: FontWeight.bold)),
-                            textColor: Theme.of(context).primaryColor,
-                          ),
+                    AdaptiveFlatButton('Choose Date', _presentDatePicker),
                   ],
                 ),
               ),
